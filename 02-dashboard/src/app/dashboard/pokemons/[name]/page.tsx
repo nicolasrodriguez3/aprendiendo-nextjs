ab07626f286ef324a8d992a4372e78a3cb3e8c9a
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2025-08-06 09:12:09 -0300

    Clase 70

--- 02-dashboard/src/app/dashboard/pokemons/[name]/page.tsx
+++ 02-dashboard/src/app/dashboard/pokemons/[name]/page.tsx
@@ -1,26 +1,29 @@
-import type { Pokemon } from "@/pokemons"
+import type { Pokemon, PokemonsResponse } from "@/pokemons"
 import { Metadata } from "next"
 import Image from "next/image"
 import { notFound } from "next/navigation"
 
 interface Props {
     params: {
-        id: string
+        name: string
     }
 }
 
 export async function generateStaticParams() {
-    const static151Pokemons = Array.from({ length: 151 }, (_, i) => i + 1)
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`, { cache: "force-cache" })
 
-    return static151Pokemons.map( id => ({id}))
+    const pokemons: PokemonsResponse = await data.json()
+
+    return pokemons.results.map(pokemon => ({ name: pokemon.name }))
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const { id } =  params
-    const pokemon = await getPokemon(id)
+    const { name } = await params
+    const pokemon = await getPokemon(name)
 
-     const pokemonId = (pokemon.id).toString().padStart(3, '0')
+    const pokemonId = (pokemon.id).toString().padStart(3, '0')
     const pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+
 
 
     return {
@@ -30,8 +33,8 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 
-const getPokemon = async (id: string): Promise<Pokemon> => {
-    const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, { cache: "force-cache" })
+const getPokemon = async (name: string): Promise<Pokemon> => {
+    const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, { cache: "force-cache" })
 
     if (!pokemonResponse.ok) {
         notFound()
@@ -43,8 +46,8 @@ const getPokemon = async (id: string): Promise<Pokemon> => {
 }
 
 export default async function PokemonPage({ params }: Props) {
-    const { id } = await params
-    const pokemon = await getPokemon(id);
+    const { name } = await params
+    const pokemon = await getPokemon(name);
 
     return (
         <div className="flex w-full mt-5 flex-col items-center text-slate-800">
--- 02-dashboard/src/app/dashboard/pokemons/components/PokemonCard.tsx
+++ 02-dashboard/src/app/dashboard/pokemons/components/PokemonCard.tsx
@@ -15,7 +15,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
             <button className="text-red-600 hover:scale-110 active:scale-95 transition-transform duration-150 absolute top-2 right-2">
                 <IoHeartOutline size={24} />
             </button>
-            <Link href={`/dashboard/pokemon/${id}`} className="flex items-center justify-center w-full h-32 mb-2">
+            <Link href={`/dashboard/pokemons/${name}`} className="flex items-center justify-center w-full h-32 mb-2">
                 <Image className="rounded-t-lg"
                     key={id}
                     src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
@@ -28,7 +28,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
                 <a href="#">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize">{name}</h5>
                 </a>
-                <Link href={`/dashboard/pokemon/${id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <Link href={`/dashboard/pokemons/${name}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Read more
                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
