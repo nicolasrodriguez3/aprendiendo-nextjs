e130c8d9031298036c3e8d039db925152d6b64e0
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2025-09-06 19:00:59 -0300

    feat: implementar redirección a dashboard en la página de inicio y agregar soporte para roles en el modelo de usuario

--- 03-admin-todos/src/auth.ts
+++ 03-admin-todos/src/auth.ts
@@ -9,4 +9,35 @@ import GitHub from "next-auth/providers/github"
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google, GitHub],
+
+  session: {
+    strategy: "jwt",
+  },
+
+  callbacks: {
+    async signIn() {
+      return true
+    },
+
+    async jwt({ token }) {
+      const dbUser = await prisma.user.findUnique({
+        where: {
+          email: token.email ?? "no-email",
+        },
+      })
+
+      token.roles = dbUser?.roles ?? []
+      token.id = dbUser?.id ?? ""
+
+      return token
+    },
+
+    async session({ session, token }) {
+      if (session && session.user) {
+        session.user.roles = token.roles
+        session.user.id = token.id
+      }
+      return session
+    },
+  },
 })
