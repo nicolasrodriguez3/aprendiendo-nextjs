911d27dadeedefa4652106c2b552e4be4f9087f0
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2025-09-09 18:26:46 -0300

    feat: agregar relación entre usuarios y tareas en el modelo Todo, incluyendo migración y ajuste en la creación de usuarios

--- 03-admin-todos/src/app/api/seed/route.ts
+++ 03-admin-todos/src/app/api/seed/route.ts
@@ -1,17 +1,27 @@
 import prisma from "@/lib/prisma"
+import bcrypt from "bcryptjs"
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
   await prisma.todo.deleteMany()
+  await prisma.user.deleteMany()
 
-  await prisma.todo.createMany({
-    data: [
-      { description: "Aprender Next" },
-      { description: "Crear un proyecto de prueba", complete: true },
-      { description: "Aprender Python" },
-      { description: "Crear una API" },
-    ],
+  await prisma.user.create({
+    data: {
+      email: "test1@google.com",
+      password: bcrypt.hashSync("123456"),
+      todos: {
+        create: [
+          { description: "Aprender Next" },  
+          { description: "Crear un proyecto de prueba", complete: true },
+          { description: "Aprender Python" },
+          { description: "Crear una API" }
+        ]
+      }
+    }
   })
+
+
 
   return NextResponse.json({
     message: "ok",
